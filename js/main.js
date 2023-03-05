8ee54ae06b36c04ad52f35971021fc0921e5482a
Rakib <rakibuzzaman1177@gmail.com>
2023-03-05 16:08:53 +0600

    new html page add

---
+++ js/main.js
@@ -0,0 +1,18 @@
+ 
+ 
+ 
+ var mainImage = document.getElementById("mainImage");
+ var smallImg = document.getElementsByClassName("small-img");
+
+ smallImg[0].onclick= function(){
+    mainImage.src = smallImg[0].src
+ }
+ smallImg[1].onclick= function(){
+    mainImage.src = smallImg[1].src
+ }
+ smallImg[2].onclick= function(){
+    mainImage.src = smallImg[2].src
+ }
+ smallImg[3].onclick= function(){
+    mainImage.src = smallImg[3].src
+ }
